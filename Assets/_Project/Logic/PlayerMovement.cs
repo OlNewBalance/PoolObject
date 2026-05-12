7c64d345aff4fbb736f086f386c0608b16f85391
Oleg <finkton23@gmail.com>
2026-05-13 00:17:13 +0500

    Update PlayerMovement.cs

--- Assets/_Project/Logic/PlayerMovement.cs
+++ Assets/_Project/Logic/PlayerMovement.cs
@@ -16,14 +16,6 @@ public class PlayerMovement : MonoBehaviour
         gameObject.transform.position = Vector3.MoveTowards(transform.position, MoveDirection(inputDirection), _moveSpeed * Time.deltaTime);
     }
 
-    //public void Looking(ref Vector2 inputDirection)
-    //{
-    //    gameObject.transform.rotation = Quaternion.EulerRotation();
-    //    gameObject.transform.rotation = Quaternion.LookRotation(LookDirection2(inputDirection));
-    //    Quaternion a = Quaternion.LookRotation(LookDirection2(inputDirection));
-    //    gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, LookDirection(inputDirection), _lookSpeed * Time.deltaTime);
-    //}
-
     public void Jumping()
     {
         _direction.y = transform.position.y + _jumpHeight;
@@ -39,42 +31,4 @@ public class PlayerMovement : MonoBehaviour
         Vector3 direction = new Vector3(_direction.x, 0, _direction.z);
         return direction;
     }
-
-//    private Quaternion LookDirection(Vector2 lookDirection)
-//    {
-//        Quaternion direction;
-//        Ray ray = _camera.ScreenPointToRay(lookDirection);
-
-//        if (Physics.Raycast(ray, out RaycastHit hit))
-//        {
-//            Vector2 vector = hit.transform.position;
-
-//            float rotationX = vector.x;
-//            float rotationY = vector.y;
-
-//            direction = Quaternion.Euler(rotationX, rotationY, 0);
-//            return direction;
-//        }
-
-//        return Quaternion.identity;
-//    }
-
-//    private Vector3 LookDirection2(Vector2 lookDirection)
-//    {
-//        Vector3 direction;
-//        Ray ray = _camera.ScreenPointToRay(lookDirection);
-
-//        if (Physics.Raycast(ray, out RaycastHit hit))
-//        {
-//            Vector2 vector = hit.transform.position;
-
-//            float rotationX = vector.x;
-//            float rotationY = vector.y;
-
-//            direction = new Vector3(rotationX,rotationY, 0);
-//            return direction;
-//        }
-
-//        return Vector3.zero;
-//    }
 }
